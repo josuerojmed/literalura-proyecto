2ec53bace14a5b1a0347aba4943ca2dc90caaec1
josuerojmed <135179823+josuerojmed@users.noreply.github.com>
2025-01-21 15:26:20 -0600

    Update LiteraturaApplication.java

--- LiteraturaApplication.java
+++ LiteraturaApplication.java
@@ -1,7 +1,7 @@
-package com.alejobeliz.proyectos.literatura;
+package com.sebasmedina.proyectos.literatura;
 
-import com.alejobeliz.proyectos.literatura.principal.Principal;
-import com.alejobeliz.proyectos.literatura.service.MenuService;
+import com.sebasmedina.proyectos.literatura.principal.Principal;
+import com.sebasmedina.proyectos.literatura.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,16 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraturaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraturaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraturaApplication.class, args);
+    }
 
-	@Autowired
-	private MenuService menuService;
+    @Autowired
+    private MenuService menuService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(menuService);
-		principal.EjecutarAplicacion();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(menuService);
+        principal.EjecutarAplicacion();
+    }
 }
