024a2759329f9549a3744c3625b3ae8d68559398
josuerojmed <135179823+josuerojmed@users.noreply.github.com>
2025-01-21 15:25:45 -0600

    Update LibroService.java

--- LibroService.java
+++ LibroService.java
@@ -1,13 +1,14 @@
-package com.alejobeliz.proyectos.literatura.service;
+package com.sebasmedina.proyectos.literatura.service;
 
-import com.alejobeliz.proyectos.literatura.model.Idioma;
-import com.alejobeliz.proyectos.literatura.model.Libro;
-import com.alejobeliz.proyectos.literatura.repository.LibroRepository;
+import com.sebasmedina.proyectos.literatura.model.Idioma;
+import com.sebasmedina.proyectos.literatura.model.Libro;
+import com.sebasmedina.proyectos.literatura.repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class LibroService {
 
@@ -18,19 +19,20 @@ public class LibroService {
         this.libroRepository = repository;
     }
 
-    public LibroService(){};
+    public LibroService() {}
 
     public List<Libro> obtenerTodosLosLibros() {
         return libroRepository.findAll();
     }
 
-    public Optional<Libro> obtenerLibroPorNombre(String nombre){
+    public Optional<Libro> obtenerLibroPorNombre(String nombre) {
         return libroRepository.obtenerLibroPorNombre(nombre);
     }
 
-    public List<Libro>obtenerLibrosPorIdioma(Idioma idioma){
+    public List<Libro> obtenerLibrosPorIdioma(Idioma idioma) {
         return libroRepository.obtenerLibrosPorIdioma(idioma);
     }
+}
 
     public void guardarLibro(Libro libro){
         libroRepository.save(libro);
