c065f774e07fa547aa63a7aabefa0dab591ca515
josuerojmed <135179823+josuerojmed@users.noreply.github.com>
2025-01-21 15:17:41 -0600

    Update AutorService.java

--- AutorService.java
+++ AutorService.java
@@ -1,12 +1,13 @@
-package com.alejobeliz.proyectos.literatura.service;
+package com.sebasmedina.proyectos.literatura.service;
 
-import com.alejobeliz.proyectos.literatura.model.Autor;
-import com.alejobeliz.proyectos.literatura.repository.AutorRepository;
+import com.sebasmedina.proyectos.literatura.model.Autor;
+import com.sebasmedina.proyectos.literatura.repository.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class AutorService {
 
@@ -17,22 +18,22 @@ public class AutorService {
         this.autorRepository = autorRepository;
     }
 
-    public AutorService(){};
+    public AutorService() {}
 
-    public Optional<Autor> obtenerAutorPorNombre(String nombre){
-         return autorRepository.obtenerAutorPorNombre(nombre);
+    public Optional<Autor> obtenerAutorPorNombre(String nombre) {
+        return autorRepository.obtenerAutorPorNombre(nombre);
     }
 
-    public void guardarAutor(Autor autor){
+    public void guardarAutor(Autor autor) {
         autorRepository.save(autor);
     }
 
-    public List<Autor> obtenerTodosLosAutores(){
+    public List<Autor> obtenerTodosLosAutores() {
         return autorRepository.findAll();
     }
 
-    public List<Autor> obtenerAutoresVivosEnAnio(int anio){
+    public List<Autor> obtenerAutoresVivosEnAnio(int anio) {
         return autorRepository.obtenerAutoresVivosEnAnio(anio);
     }
-
 }
+
